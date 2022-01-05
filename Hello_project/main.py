a978d1c80d3d5753b5f558b4217407039d68f99d
Dietmar <tcodmos@gmail.com>
2022-01-05 22:24:49 +0300

    first changes

--- Hello_project/main.py
+++ Hello_project/main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,4 +8,22 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("F")
+
+    number = int(input("Enter number: "))
+    if number > 100:
+        print("More, than 100, current number: ", number)
+    elif 50 <= number <= 100:
+        print("Between 50 to 100, current number: ", number)
+    else:
+        print("Less, than 50, current number: ", number)
+
+    name = input("Enter name: ")
+    res = 1000 if "Dietrich" == name else 100
+    print(res)
+
+    variable = 666
+    print(variable)
+    variable = "Santa-sa-santa-y-satan"
+    print(variable)
+
