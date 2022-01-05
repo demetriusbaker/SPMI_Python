08d67171ae3aa3018f53f12e9919e9fb33ea199f
Dietmar <tcodmos@gmail.com>
2022-01-05 13:50:30 +0300

    init project

---
+++ First steps/separate_code_file.py
@@ -0,0 +1,13 @@
+name = input("What's your name? ")
+print ("Hi,", name)
+
+a = 5
+b = 3
+print (a ** b)
+
+num1 = int(input("Input number1: "))
+num2 = int(input("Input number2: "))
+print("Sum of num1 and num2: ", num1 + num2)
+
+num = int(input())
+if (num == 666): print("HAAHAHHAHAHAHHAHAHAHHAHAHAHH!")
